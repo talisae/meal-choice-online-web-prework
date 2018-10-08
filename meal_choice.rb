f5757b49d38dfd5883243fe7ef2b9fb8bbb3216d
Talisa Erazo <talisaerazo@gmail.com>
2018-10-08 02:59:44 +0000

    automatically backed up by learn

--- meal_choice.rb
+++ meal_choice.rb
@@ -1,5 +1,5 @@
-def meal_choice (meal_choice("vegan"), meal_choice = "meat")
+def meal_choice (meal_choice = "meat")
   puts "meat"
+  "#{meal_choice = vegan}"
   "#{meal_choice = "meat"}"
-  "#{meal_choice("vegan") = "vegan"}"
 end
